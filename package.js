3d73f64b2d6dc6c581d6bb70fd18e567e1d79508
Derek Caneja <derek@dispatch.me>
2015-10-05 15:45:00 -0400

    Bump and release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dispatch:events-pan',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Touch event listener and pan event emitter',
   git: 'https://github.com/DispatchMe/meteor-events-pan.git'
 
